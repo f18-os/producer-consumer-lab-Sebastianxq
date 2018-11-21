7d4ffce4d2390847b913b4c54f808c6bcead59d9
student <Sebsatianq1290@gmail.com>
2018-11-20 17:51:53 -0700

    cleaned code, missing exit condition

--- lab.py
+++ lab.py
@@ -15,35 +15,20 @@ fullSem2 = threading.Semaphore() #full queue for display
 lock2 = threading.Semaphore(0) #mutex for display
 
 def extractFrames(fileName, extractionQueue):
-    
     print("start extract frames!")
-    # Initialize frame count 
-    count = 0
+     
+    count = 0 #frame count
     
-    # open video file
-    vidcap = cv2.VideoCapture(fileName)
+    vidcap = cv2.VideoCapture(fileName) #open video file
 
-    # read first image
-    success,image = vidcap.read()
+    success,image = vidcap.read() #read first image
     
     print("Reading frame {} {} ".format(count, success))
     while success:
-    #while success and not extractionQueue.full():
 
-        # get a jpg encoded frame
-        success, jpgImage = cv2.imencode('.jpg', image)
-
-        #debugging, ensures a picture is actually there
-        #cv2.imshow("extractImg",jpgImage)
-        
-        
-        #encode the frame as base 64 to make debugging easier
-        jpgAsText = base64.b64encode(jpgImage)
-        
-        # add the frame to the buffer
+        #Producer 1, stores frame into queue
         emptySem.acquire()
         #lock.acquire()
-        #extractionQueue.put(jpgAsText)
         extractionQueue.put(image)
         #lock.release()
         fullSem.release()
@@ -60,39 +45,20 @@ def convertToGray(extractionQueue, displayQueue):
      count = 0 #initialize frame count
      
      while True:
-     #while not extractionQueue.empty() or not displayQueue.full():
      
-       #CONSUMER1, takes extracted frame
+       #Consumer 1, takes extracted frame
        fullSem.acquire()
-       frameAsText = extractionQueue.get()
+       colorFrame = extractionQueue.get()
        emptySem.release()
 
-       #consumer1 debugger, seems like it works
-       #print("below is what is taken from queue1")
-       #print(frameAsText)
-       
-       # decode the frame 
-       jpgRawImage = base64.b64decode(frameAsText)
-
-       # convert the raw frame to a numpy array
-       jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
-        
-       #get a jpg encoded frame
-       img = cv2.imdecode(jpgImage ,cv2.IMREAD_UNCHANGED)
-       
        print("Converting frame {}".format(count))
         
-       # convert the image to grayscale
-       #originally img
-       greyFrame = cv2.cvtColor(frameAsText, cv2.COLOR_BGR2GRAY)
-
-       #encode the frame as base 64 to make debugging easier
-       jpgAsText = base64.b64encode(greyFrame)
+       #convert the frame to grayscale
+       greyFrame = cv2.cvtColor(colorFrame, cv2.COLOR_BGR2GRAY)
        
-       #PRODUCER, stores to display
+       #Producer 2, stores to display
        emptySem2.acquire()
        #lock2.acquire()
-       #displayQueue.put(jpgAsText)
        displayQueue.put(greyFrame)
        #lock2.release()
        fullSem2.release()
@@ -104,39 +70,22 @@ def convertToGray(extractionQueue, displayQueue):
        
 
 def displayFrames(displayQueue):
-    print("start display! ") 
+    print(" start display! ") 
     
     count = 0 #frameCount
 
     while True:
-    #while not displayQueue.empty():
-        #if not displayQueue.empty():
         
-           #CONSUMER 2, gets frame to display
+           #Consumer 2, gets frame to display
            fullSem2.acquire()
            #lock2.acquire()
            frameAsText = displayQueue.get()
            #lock2.release()
            emptySem2.release()
 
-           # decode the frame 
-           jpgRawImage = base64.b64decode(frameAsText)
-
-           # convert the raw frame to a numpy array
-           jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
-        
-           # get a jpg encoded frame
-           img = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
-
            print("Displaying frame {}".format(count))        
 
-           
-           #print(img)
-           # display the image in a window called "video" and wait 42ms
-           # before displaying the next frame
-           #if img is not None:
-           #print("really showing the image!")
-           #cv2.imshow("Video", img)
+           #display the image in a window called "video" and wait 42ms
            cv2.imshow("Video", frameAsText)
            if cv2.waitKey(42) and 0xFF == ord("q"):
                break
@@ -171,13 +120,10 @@ displayT.start()
 #not encoding it works with extract-> display
 
 #ISSUES
-#Need to fix GREYSCALE CONVERSION
-#Need to fix  LOCKS
+#Need to fix LOCKS
 #Need to add an exit protocol when it's done
 #include the semophores within call and put to simplify the process?
 
-#Extract/Display work with Sems and any size que
-   #need to test out with lock??
 #went over sem implementation on Oct23
 
 
